50bdbdaa7830ba5380f668a9ebf84052ce52c46e
Emil Szymczyk <emil.szymczyk@ankork.pl>
2024-06-10 20:32:54 +0200

    feat(frontend): timeline

--- nextjs/components/OurProcess/components/Timeline/Timeline.tsx
+++ nextjs/components/OurProcess/components/Timeline/Timeline.tsx
@@ -1,6 +1,7 @@
+"use client";
 import { i18n } from "@/i18n/helpers";
 import { LangType } from "@/types/types";
-import { Button, Timeline as MantineTimeline, Text, TimelineItem } from "@mantine/core";
+import { Button, Flex, Timeline as MantineTimeline, Text, TimelineItem } from "@mantine/core";
 import {
   IconGitBranch,
   IconGitPullRequest,
@@ -10,26 +11,41 @@ import {
 import Link from "next/link";
 import { ContentSchema } from "@/i18n/schema";
 import { nanoid } from "nanoid";
+import { useState } from "react";
+import css from "./Timeline.module.css";
 
 type TimelineKeys = keyof ContentSchema["offer"]["ourProcess"]["timeline"];
 type TimelineProps = {
   lang: LangType;
 };
 export const Timeline = ({ lang }: TimelineProps) => {
+  const [activeItem, setActiveItem] = useState(0);
   const timeLineData = i18n(lang).offer.ourProcess.timeline;
   //TODO: icon pick depends on timelineData[step] name ex. if .. ===deliverproduct => icon
   return (
-    <MantineTimeline active={1} bulletSize={24} lineWidth={2}>
-      {Object.keys(timeLineData).map((step) => (
+    <MantineTimeline
+      active={activeItem}
+      bulletSize={66}
+      lineWidth={2}
+      classNames={{ itemTitle: css.itemTitle }}
+    >
+      {Object.keys(timeLineData).map((step, index) => (
         <TimelineItem
+          onClick={() => setActiveItem(index)}
           key={nanoid()}
-          bullet={<IconGitBranch size={12} />}
+          bullet={<IconGitBranch size={42} />}
           title={timeLineData[step as TimelineKeys].name}
+          className={`${css.timeline} ${activeItem >= index ? css["timeline-active"] : ""}`}
         >
-          <Text c="dimmed" size="sm">
-            {timeLineData[step as TimelineKeys].description}
-          </Text>
-          <Button component={Link} href={"/"} variant="transparent">
+          <Flex w={"100%"} justify={"space-between"}>
+            <Text fw={400} fz={16} maw={840}>
+              {timeLineData[step as TimelineKeys].description}
+            </Text>
+            <Text fw={700} fz={24}>
+              {timeLineData[step as TimelineKeys].category}
+            </Text>
+          </Flex>
+          <Button fz={16} fw={700} component={Link} href={"/"} variant="transparent">
             {timeLineData[step as TimelineKeys].btn}
           </Button>
         </TimelineItem>
